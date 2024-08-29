ad40643a8376411f20197ddd1b5117c68e48c50e
bauhoffer mariano <bauhoffermariano@gmail.com>
2024-08-29 11:08:35 -0300

    aa

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,12 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
     plugins: [ require('autoprefixer')]
-}
+},
+module.exports = {
+    plugins: [
+      purgecss({
+        content: ['./**/*.html']
+      })
+    ]
+  }
